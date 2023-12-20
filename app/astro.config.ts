d13b44f502ea837230ee40078f19dcc223e68b90
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-12-20 12:15:28 -0300

    build(astro): :wrench: adds cache, ssr (with node adapter) and internationalization configs

--- app/astro.config.ts
+++ app/astro.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "astro/config";
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: "https://www.edsonpimenta.net",
-  trailingSlash: "never",
+  trailingSlash: "always",
+  cacheDir: "./cache",
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pt-br"],
+    routing: {
+      strategy: "pathname",
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      "pt-br": "en",
+    },
+  },
 });
