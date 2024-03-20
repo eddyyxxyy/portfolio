fd37176cc0535c174dbb56d15ef4eb3d9ce29523
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-03-20 17:25:57 -0300

    feat(ui): :lipstick: implements `Header` and translations with theme & lang switch on all pages

---
+++ src/i18n/404.ts
@@ -0,0 +1,14 @@
+const translations404 = {
+  en: {
+    pageTitle: "Page not found!",
+    pageDescription: "The page you're trying to access doesn't exist.",
+    h1: "Page not found!",
+  },
+  "pt-br": {
+    pageTitle: "Página não encontrada!",
+    pageDescription: "A página que você está tentando acessar não existe.",
+    h1: "Página não encontrada!",
+  },
+};
+
+export { translations404 };
---
+++ src/i18n/Header.ts
@@ -0,0 +1,28 @@
+const translationsHeader = {
+  en: {
+    logoAlt: "eddyyxxyy's logo that consists of an abstract illustration of two minimalistic faces looking to opposite ways.",
+    menuToggle: "Toggle Navigation Menu.",
+    about: "About",
+    career: "Career",
+    projects: "Projects",
+    colorMode: "Select site theme",
+    langLabel: "Select your preferred language",
+    lang: "English",
+    lightModeOption: "Light",
+    darkModeOption: "Dark",
+  },
+  "pt-br": {
+    logoAlt: "Logo do portfólio de eddyyxxyy. Consiste numa ilustração abstrata de dois rostos minimalistas olhando para lados opostos.",
+    menuToggle: "Abrir Menu de Navegação.",
+    about: "Sobre mim",
+    career: "Carreira",
+    projects: "Projetos",
+    colorMode: "Escolha o tema do site",
+    langLabel: "Escolha seu idioma de preferência",
+    lang: "Portuguese",
+    lightModeOption: "Claro",
+    darkModeOption: "Escuro",
+  },
+};
+
+export { translationsHeader };
