1c0399fb173fe948ec4c886841eb92bacfba37e9
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-07-02 13:12:30 -0300

    feat(ui): :lipstick: creates and implements `404.astro` page with `LinkButton` component

--- src/i18n/404.ts
+++ src/i18n/404.ts
@@ -3,11 +3,15 @@ const translations404 = {
     pageTitle: "Page not found!",
     pageDescription: "The page you're trying to access doesn't exist.",
     h1: "Page not found!",
+    p: "Oops... It seems like this page doesn't exist!",
+    link: "Go back to Home",
   },
   "pt-br": {
     pageTitle: "Página não encontrada!",
     pageDescription: "A página que você está tentando acessar não existe.",
     h1: "Página não encontrada!",
+    p: "Ops... Parece que essa página não existe!",
+    link: "Voltar para a Home",
   },
 };
 
