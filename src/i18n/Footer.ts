a016d486a38bb7930181b70c5694f61c0544da54
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-07-12 12:59:28 -0300

    feat(i18n): :globe_with_meridians: adds pt-br translations to  component

---
+++ src/i18n/Footer.ts
@@ -0,0 +1,14 @@
+const translationsFooter = {
+  en: {
+    h1: "Connect with me",
+    resumeLink: "Download resume",
+    repositoryLink: "Project repository",
+  },
+  "pt-br": {
+    h1: "Conecte-se comigo",
+    resumeLink: "Baixar currículo",
+    repositoryLink: "Repositório do projeto",
+  },
+};
+
+export { translationsFooter };
