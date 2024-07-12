79e1f20d094307de132071822b5f74935f63d8cd
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-07-12 13:31:57 -0300

    fix(i18n): :adhesive_bandage: changes  between pt-br and en pages to download resume

--- src/i18n/Footer.ts
+++ src/i18n/Footer.ts
@@ -1,11 +1,13 @@
 const translationsFooter = {
   en: {
     h1: "Connect with me",
+    resumeHref: "/Edson_Pimenta_resume.pdf",
     resumeLink: "Download resume",
     repositoryLink: "Project repository",
   },
   "pt-br": {
     h1: "Conecte-se comigo",
+    resumeHref: "/Edson_Pimenta_curriculo.pdf",
     resumeLink: "Baixar currículo",
     repositoryLink: "Repositório do projeto",
   },
