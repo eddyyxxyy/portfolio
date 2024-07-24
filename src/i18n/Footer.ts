3dbd5f8bc9d5b8c12810e68864f08c719a806242
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-07-24 11:13:42 -0300

    fix(seo): :adhesive_bandage: adds  to s and removes unused vars

--- src/i18n/Footer.ts
+++ src/i18n/Footer.ts
@@ -4,12 +4,18 @@ const translationsFooter = {
     resumeHref: "/Edson_Pimenta_resume.pdf",
     resumeLink: "Download resume",
     repositoryLink: "Project repository",
+    githubLinkAriaLabel: "Edson's Github profile page",
+    linkedinLinkAriaLabel: "Edson's LinkedIn profile page",
+    youtubeChannelLinkAriaLabel: "Edson's Youtube channel",
   },
   "pt-br": {
     h1: "Conecte-se comigo",
     resumeHref: "/Edson_Pimenta_curriculo.pdf",
     resumeLink: "Baixar currículo",
     repositoryLink: "Repositório do projeto",
+    githubLinkAriaLabel: "Perfil do Github do Edson",
+    linkedinLinkAriaLabel: "Perfil do LinkedIn do Edson",
+    youtubeChannelLinkAriaLabel: "Canal no Youtube do Edson",
   },
 };
 
