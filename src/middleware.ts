ae53fdcdd0aa9c4f4e7ba3f3efe874ea22b0c782
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-08-06 13:55:04 -0300

    chore(version): :bookmark: bumps app version to 0.2.2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  context.locals.appVersion = "0.2.1";
+  context.locals.appVersion = "0.2.2";
 
   return next();
 });
